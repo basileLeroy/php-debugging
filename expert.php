11bed29bb6580b57396590e0a4c64c4fd5054330
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-01-04 15:43:05 +0100

    EXPERT.php: Debugging ex02

--- expert.php
+++ expert.php
@@ -8,9 +8,18 @@ declare(strict_types=1);
     // sometimes, even your IDE can tell you what's wrong
     echo "Exercise 1 starts here:";
     
-
     function new_exercise() {
         $x = 1;
         $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
         echo $block;
     }
+
+    new_exercise(2);
+    // === Exercise 2 ===
+    // Below we create a week array with all days of the week.
+    // We then try to print the first day which is monday, execute the code and see what happens.
+
+    $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    $monday = $week[0];
+
+    echo $monday;
