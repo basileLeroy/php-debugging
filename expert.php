e8d67fb84888851959098f95f84ab6ef092ab0af
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-01-04 15:58:13 +0100

    EXPERT.php: Debugging ex03

--- expert.php
+++ expert.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
     // sometimes, even your IDE can tell you what's wrong
     echo "Exercise 1 starts here:";
     
-    function new_exercise() {
-        $x = 1;
+    function new_exercise($x) {
+        
         $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
         echo $block;
     }
@@ -23,3 +23,11 @@ declare(strict_types=1);
     $monday = $week[0];
 
     echo $monday;
+
+
+    new_exercise(3);
+    // === Exercise 3 ===
+    // This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+    $str = "Debugged ! Also very fun";
+    echo substr($str, 0, 10);
